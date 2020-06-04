aea0d02f50e31f35009c3ea538b74b9f8d7c1390
aaltmark <adamaltmark@gmail.com>
2020-06-04 16:02:47 -0400

    Adam updates

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Owner
   attr_accessor
   attr_reader :name, :species
@@ -9,10 +10,6 @@ class Owner
     @species = "human"
     @@all << self
   end
-
-  # def name
-  #   @name
-  # end
 
 def say_species
   p "I am a #{species}."
@@ -30,6 +27,40 @@ def self.reset_all
   @@all.clear
 end
 
+def cats
+  Cat.all.select do |cat|
+    cat.owner == self 
+  end
+end 
 
+def dogs
+  Dog.all.select do |dog|
+    dog.owner == self 
+  end
+end
+
+def buy_cat(name)
+  name = Cat.new(name, self)
+  #binding.pry 
+end
+
+def buy_dog(name)
+  name = Dog.new(name, self)
+end
+
+def walk_dogs 
+  self.dogs.each do |dog| 
+  dog.mood = "happy"
+end 
+end 
+
+def feed_cats 
+  self.cats.each do |cat| 
+  cat.mood = "happy"
+end 
+end 
 
 end
+
+#joe = Owner.new("Joe")
+#binding.pry 
